b5e68bb0945910d7c3d0129734587f00c49e8cff
Danny Davila <danny@misaki.tech>
2023-10-24 08:18:44 -0500

    [Added] Pages Structure

---
+++ app/ClientMember/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ClientMember = () => {
+  return (
+    <div>
+      <h1>Client Member</h1>
+    </div>
+  );
+};
+
+export default ClientMember;
---
+++ app/Member/page.tsx
@@ -0,0 +1,9 @@
+const Member = () => {
+  return (
+    <div>
+      <h1>Server Member Session</h1>
+    </div>
+  );
+};
+
+export default Member;
